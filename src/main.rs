176f2a128ab8718b59d75523665814d2cd6bac30
mnasie <manassehsamuel642@gmail.com>
2026-08-28 15:45:40 +0300

    Add multi-threaded client handling
    Process incoming connections in separate threads to allow concurrent
    server operations and track the total number of connected clients.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn handle_clients(mut stream: TcpStream) {
     let mut buf = [0u8; 1024];
@@ -18,11 +19,16 @@ fn handle_clients(mut stream: TcpStream) {
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
     let srvr = TcpListener::bind("127.0.0.1:9000")?;
+    let mut count_client = 0;
 
     for stream in srvr.incoming() {
         match stream {
             Ok(s) => {
-                handle_clients(s);
+                count_client += 1;
+                println!("Client {} connected", count_client);
+                thread::spawn(move || {
+                    handle_clients(s);
+                });
             }
             Err(_) => {
                 println!("Socket accept failure!")
